26bca2eef796d937da8515cd10992aa5fa0879aa
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2021-07-12 17:47:21 -0500

    added navlinks to pages

--- app/javascript/components/components/Header.js
+++ app/javascript/components/components/Header.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Card, CardTitle, Col, Button } from 'reactstrap'
+import { NavLink } from 'react-router-dom'
 
 
 class Header extends Component{
@@ -13,7 +15,10 @@ class Header extends Component{
                 <h3>Header</h3>
                 { logged_in && <a href={ sign_out_route }>Sign Out</a> }
                 {!logged_in && <a href={ sign_in_route }>Sign In</a>}
+                <br></br>
+                {this.props.logged_in && <NavLink to="./userrecipes"> Go to your Recipes Page!</NavLink>}
             </header>
+        
         )
     }
 }
